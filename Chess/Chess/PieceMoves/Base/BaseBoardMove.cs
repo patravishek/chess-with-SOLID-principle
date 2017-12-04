11044770d545d0bf9d1e5832782f7ea1cc5b8e06
AP7392 <AP7392@AP7392-VM1>
2017-12-04 16:17:36 +0530

    Added Logic for King Move and Pawn Move with Test Cases

---
+++ Chess/Chess/PieceMoves/Base/BaseBoardMove.cs
@@ -0,0 +1,40 @@
+﻿using Chess.Implementation;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Chess.PieceMoves
+{
+    public abstract class BasePieceMoves
+    {
+        public abstract List<string> Moves(string currentPosition);
+
+        protected ChessBoard chess = ChessBoard.Instance;
+
+        protected List<string> GettingActualPossibleMove(List<string> inputValue)
+        {
+            var possibleMove = new List<string>();
+
+            inputValue.ForEach(_item =>
+            {
+                if (!chess.BoardMap[Convert.ToInt32(_item)].Item2)
+                {
+                    possibleMove.Add(chess.BoardMap[Convert.ToInt32(_item)].Item1);
+                }
+            });
+            
+            return possibleMove;
+        }
+
+        /// <summary>
+        /// This method is not implemented, this can be implemented in case user selects a paritcular piece and wants to move its position
+        /// </summary>
+        /// <param name="pieceName"></param>
+        /// <param name="cell"></param>
+        /// <returns></returns>
+        protected bool UpdateMove(string pieceName, string cell)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Chess/Chess/PieceMoves/KingMove.cs
@@ -0,0 +1,33 @@
+﻿using Chess.Constants;
+using System;
+using System.Collections.Generic;
+
+namespace Chess.PieceMoves
+{
+    public class KingMove : BasePieceMoves
+    {
+        /// <summary>
+        /// King can move various direction
+        /// </summary>
+        /// <param name="currentPosition"></param>
+        /// <returns></returns>
+        public override List<string> Moves(string currentPosition)
+        {
+            var currentPositionIndex = chess.BoardMap.IndexOf(chess.BoardMap.Find(value => value.Item1.Equals(currentPosition)));
+            var nextPossibleMove = new List<string>
+            {
+                Convert.ToString(currentPositionIndex + 1),//E5
+                Convert.ToString(currentPositionIndex - 1),//C5
+                Convert.ToString(currentPositionIndex + Constant.Row),//D4
+                Convert.ToString(currentPositionIndex + (Constant.Row + 1)),//E4
+                Convert.ToString(currentPositionIndex + (Constant.Row - 1)),//C4
+                Convert.ToString(currentPositionIndex - Constant.Row),//D6
+                Convert.ToString(currentPositionIndex - (Constant.Row + 1)),//C6
+                Convert.ToString(currentPositionIndex - (Constant.Row - 1))//E6
+            };
+
+            //Check if the positions are not occupied
+            return GettingActualPossibleMove(nextPossibleMove);
+        }
+    }
+}
