410a099d699318550c9d0c1c31e387a2fb09bcad
AP7392 <AP7392@AP7392-VM1>
2017-12-20 05:44:01 -0500

    Code Optimization

---
+++ Chess/Chess/PieceMoves/TypeMoves/KingTypeMove.cs
@@ -0,0 +1,15 @@
+﻿using Chess.PieceMoves.Base;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Chess.PieceMoves.TypeMoves
+{
+    public class KingTypeMove : PositionMoves
+    {
+        public override List<string> MoveIndividualTypes(string currentCellPosition)
+        {
+            return new KingMove().Moves(currentCellPosition);
+        }
+    }
+}
